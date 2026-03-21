c6ce40808fe8aef7ebb29e47658b43dfa7ea66c4
Vansh Mehta <vanshm2703@gmail.com>
2026-03-21 11:05:59 +0530

    Fix issues in hi.java
    - The class HelloWorldApp does not have a main method with a String[] parameter
    - The class HelloWorldApp does not have a Javadoc comment

--- hi.java
+++ hi.java
@@ -1,3 +1,6 @@
+/**
+ * A simple Java application that prints "Hello World" to the console.
+ */
 public class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World");
