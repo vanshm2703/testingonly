d584372510bb9e226694d6d25abd02c55d56a841
Vansh Mehta <vanshm2703@gmail.com>
2026-03-22 00:33:52 +0530

    Fix issues in hi.java
    - Missing class description
    - Method main() should be declared as public static void main(String[] args)
    - Variable args is not used
    - Missing Javadoc comments for the main method

--- hi.java
+++ hi.java
@@ -1,4 +1,12 @@
+/**
+ * A simple Java application that prints "Hello World" to the console.
+ */
 public class HelloWorldApp {
+    /**
+     * The main entry point for the application.
+     * 
+     * @param args Command line arguments (not used in this application).
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
